345784d84870546e62ae35954ec818098e5b0d09
Enzo Ristori <gamerenzo123@gmail.com>
2023-05-09 21:28:06 -0300

    Modificado para melhor funcionamento

--- definePosicoes.py
+++ definePosicoes.py
@@ -1,6 +1,10 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     lista = []
     lista.append([linha,coluna])
+    if orientacao == 1:
+        orientacao = 'vertical'
+    if orientacao == 2:
+        orientacao = 'horizontal'
     if orientacao == 'vertical':
         for i in range(1,tamanho):
             linha+=1
